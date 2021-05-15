d47662d6b6ff8fcbd3fb6002f0c684ecad75eb25
Robert Vo <7768974+robert-vo@users.noreply.github.com>
2021-05-15 16:35:26 -0500

    Create hide-mint-zero-balance.script.js

---
+++ hide-mint-zero-balance.script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         hide mint 0.00 balance
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        **https://mint.intuit.com/overview.event**
+// @icon         https://www.google.com/s2/favicons?domain=intuit.com
+// @grant        none
+// @require      file:///Users/Robert/Desktop/hide-mint-zero-balance.script.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
